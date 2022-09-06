b1b3fe1f361f305d4f40bbae17711599e1b41863
UClaudio <107913451+UClaudio@users.noreply.github.com>
2022-09-06 18:00:20 +0200

    compitoangular2def

--- m3/w2/d2/HW/compito/src/app/app.module.ts
+++ m3/w2/d2/HW/compito/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './main/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ActivePostsComponent } from './pages/active-posts/active-posts.component';
 import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- m3/w2/d2/HW/compito/src/app/pages/home/home.component.ts
+++ m3/w2/d2/HW/compito/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,23 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  newPost:Post = new Post('','');
+
+  addNewPost():void{
+    let postCopy = Object.assign({}, this.newPost)
+
+    let option = {
+      method: "POST",
+      body: JSON.stringify(postCopy),
+      headers: {
+          "content-type": "application/json"
+      }
+    }
+
+    fetch('http://localhost:3000/posts', option)
+    .then(res => res.json())
+    .then(() => {
+      this.newPost = new Post('', '')
+    })
+  }
 }
