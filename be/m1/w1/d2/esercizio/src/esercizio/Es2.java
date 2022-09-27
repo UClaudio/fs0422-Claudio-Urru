7559d117b80f00673d6b7d81e1cb3a4771c541f1
UClaudio <107913451+UClaudio@users.noreply.github.com>
2022-09-27 16:52:43 +0200

    eserciziojava2

---
+++ be/m1/w1/d2/esercizio/src/esercizio/Es2.java
@@ -0,0 +1,36 @@
+package esercizio;
+
+public class Es2 {
+
+	public static void main(String[] args) {
+		System.out.println();
+
+	}
+
+}
+
+class Call {
+	int numero;
+	int durata;
+	
+	Call(int numero, int durata){
+		this.numero = numero;
+		this.durata = durata;
+
+	}
+	
+
+}
+
+class SIM{
+	
+	int numeroTel;
+	double credito = 0;
+	double costoMin = 0.20;
+	SIM(int numeroTel){
+		this.numeroTel = numeroTel;
+	}
+	
+
+	
+}
---
+++ be/m1/w1/d2/esercizio/src/esercizio/Es3.java
@@ -0,0 +1,28 @@
+package esercizio;
+
+public class Es3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
+class Articolo {
+	int codArt;
+	String description;
+	int price;
+	int disponibilita;
+	Articolo(int codArt, String description, int price, int disponibilita){
+		this.codArt = codArt;
+		this.description = description;
+		this.price = price;
+		this.disponibilita = disponibilita;
+		
+	}
+}
+
+class Cliente {
+	int customerId;
+	
+}
